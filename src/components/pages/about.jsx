cae3b2e9185cc7346de09757742369dc81bcd423
Lasia Lo <lasiaylo@fb.com>
2022-01-07 22:46:48 -0500

    Click filmmaker for bio

--- src/components/pages/about.jsx
+++ src/components/pages/about.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useCallback, useState} from 'react'
 import "../../styles/about.sass"
 import {GatsbyImage} from "gatsby-plugin-image";
 
@@ -10,22 +10,22 @@ const getFilmmakerName = (filmmaker) => `${filmmaker.firstName} ${filmmaker.last
 
 const getPronouns = (filmmaker) => `(${filmmaker.pronouns.join('/')})`
 
-const getProfilePictures = (filmmakers) => {
+const getProfilePictures = (filmmakers, setFilmmaker) => {
     const pictures = new Array(16).fill(null)
-    for (let i = 0; i < pictures.length; i++ ) {
-        pictures[i] = getProfilePicture(filmmakers[i])
+    for (let i = 0; i < pictures.length; i++) {
+        pictures[i] = getProfilePicture(filmmakers[i], setFilmmaker)
     }
     return pictures
 }
 
-const getProfilePicture = (filmmaker) => {
-    console.log(filmmaker)
+const getProfilePicture = (filmmaker, setFilmmaker) => {
     let picture = <div className={"filmmakerFillerProfilePicture"}><span>?</span></div>
     if (filmmaker && filmmaker.profilePicture) {
         picture = <GatsbyImage
             className={"filmmakerProfilePicture"}
             alt={"Profile Picture"}
             image={filmmaker.profilePicture.gatsbyImageData}
+            onClick={() => setFilmmaker(filmmaker)}
         />
     }
     return <div className={"filmmakerProfilePictureContainer"}>{picture}</div>
@@ -33,16 +33,15 @@ const getProfilePicture = (filmmaker) => {
 
 
 export default function About({films}) {
-    console.log("RENDER")
-    console.log(films)
     const filmmakers = getFilmmakers(films)
 
     const [selectedFilmmaker, setSelectedFilmmaker] = useState(filmmakers[0])
+    const setSelectedFilmmakerCallback = useCallback((filmmaker) => setSelectedFilmmaker(filmmaker))
+
     const name = getFilmmakerName(selectedFilmmaker)
     const pronouns = getPronouns(selectedFilmmaker)
     const bio = JSON.parse(selectedFilmmaker.bio.raw).content[0].content[0].value
-    const profilePictures = getProfilePictures(filmmakers)
-
+    const profilePictures = getProfilePictures(filmmakers, setSelectedFilmmakerCallback)
 
     return (<div className={"aboutContainer"}>
             <div className={"aboutDescription"}>We are making 52 movies in one year.</div>
