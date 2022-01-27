572c88c376438f253f688cb5541609aa55bd427d
Lasia Lo <lasiaylo@fb.com>
2022-01-27 13:27:07 -0500

    Fix filmmaker bio transition

--- src/components/about/FilmmakerBio.jsx
+++ src/components/about/FilmmakerBio.jsx
@@ -12,27 +12,22 @@ export function FilmmakerBio({filmmaker}) {
     useEffect(() => {
             setHidden(true)
             setTimeout(() => {
+                setFilmmakerInfo(({
+                    name: getFilmmakerName(filmmaker),
+                    pronouns: getPronouns(filmmaker),
+                    bio: JSON.parse(filmmaker.bio.raw).content[0].content[0].value,
+                    link: (filmmaker.links === null) ? null : <div>
+                        <span className={"filmmakerLinksText"}>Links | </span>
+                        <a className={"filmmakerLink"}
+                           href={filmmaker.links.url}
+                        >{filmmaker.links.displayText}</a>
+                    </div>
+                }))
                 setHidden(false)
 
             }, 100)
         }, [filmmaker]
     )
-
-    useEffect(() => {
-        if (!hidden) {
-            setFilmmakerInfo(({
-                name: getFilmmakerName(filmmaker),
-                pronouns: getPronouns(filmmaker),
-                bio: JSON.parse(filmmaker.bio.raw).content[0].content[0].value,
-                link: (filmmaker.links === null) ? null : <div>
-                    <span className={"filmmakerLinksText"}>Links | </span>
-                    <a className={"filmmakerLink"}
-                       href={filmmaker.links.url}
-                    >{filmmaker.links.displayText}</a>
-                </div>
-            }))
-        }
-    }, [hidden])
 
     const {name, pronouns, bio, link} = filmmakerInfo
     return (
