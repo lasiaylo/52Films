b3616e4f48240733da5ba2cb7b891714cb22e4a9
Lasia Lo <lasiaylo@fb.com>
2022-01-27 22:03:32 -0500

    [Mobile] Try again to fix navbar and fix padding on archive top row

--- src/pages/index.js
+++ src/pages/index.js
@@ -183,7 +183,7 @@ export default function IndexPage({data}) {
                         </motion.div>
                     }
                     {
-                        isMobile() && <span
+                        isMobile() && <div
                             className={`navbarLine ${expanded}`}
                             key={"navbarLine"}
                         />
