3c83626dad50fc5cb51921bb754f5174fd25821d
Lasia Lo <lasiaylo@fb.com>
2022-01-27 22:41:43 -0500

    [Mobile] Archive styling

--- src/components/archive/directory.jsx
+++ src/components/archive/directory.jsx
@@ -64,34 +64,6 @@ const MemoizedListElement = memo(ListElement,
 )
 
 export default function Directory({films, selectedIndex, setSelected, setShowFilm}) {
-    var checkScrollSpeed = (function (settings) {
-        settings = settings || {};
-
-        var lastPos, newPos, timer, delta,
-            delay = settings.delay || 50; // in "ms" (higher means lower fidelity )
-
-        function clear() {
-            lastPos = null;
-            delta = 0;
-        }
-
-        clear();
-
-        return function () {
-            newPos = window.scrollY;
-            if (lastPos != null) { // && newPos < maxScroll
-                delta = newPos - lastPos;
-            }
-            lastPos = newPos;
-            clearTimeout(timer);
-            timer = setTimeout(clear, delay);
-            return delta;
-        };
-    })();
-
-// listen to "scroll" event
-    window.onscroll = () => console.log("adslksajdlsakj")
-
     return (
         <div className={"directoryContainer"}>
             <motion.div className={"directory"}>
