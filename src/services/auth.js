73aa5e089dbd44af6c02c519f680b865ecacd0c2
Lasia Lo <lasiaylo@fb.com>
2022-01-28 22:13:28 -0500

    Landscape formatting

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,7 +2,7 @@ export const isBrowser = () => typeof window !== "undefined"
 
 export function isMobile() {
     if (isBrowser()) {
-        return window.innerWidth <= 600 || window.innerHeight <=375
+        return window.innerWidth <= 600 || window.innerHeight <= 400
     }
     return false
 }
