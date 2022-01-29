971dd04ec5ed42398813db6d311175517b31d108
Lasia Lo <lasiaylo@fb.com>
2022-01-28 21:26:42 -0500

    Handle landscape

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,7 +2,7 @@ export const isBrowser = () => typeof window !== "undefined"
 
 export function isMobile() {
     if (isBrowser()) {
-        return window.innerWidth < 600
+        return window.innerWidth <= 600 || window.innerHeight <=375
     }
     return false
 }
